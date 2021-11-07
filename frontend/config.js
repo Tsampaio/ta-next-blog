51d2a7785357e58ea6fb36654532f6f6099257c1
Telmo Sampaio <telmoasampaio@gmail.com>
2021-11-07 21:48:52 +0000

    single blog with SEO head section

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,13 @@
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.API_PRODUCTION : publicRuntimeConfig.API_DEVELOPMENT;
+export const API = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.API_PRODUCTION
+  : publicRuntimeConfig.API_DEVELOPMENT;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
+
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.DOMAIN_PRODUCTION
+  : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+
+export const FB_AP_ID = publicRuntimeConfig.FB_AP_ID;
